c1afe720dc2d0e3f07173e20e360600e7fbe1c4f
gizmore <gizmore@wechall.net>
2022-07-22 13:59:40 +0200

    '7.0.0-r1380'

--- Module_Facebook.php
+++ Module_Facebook.php
@@ -10,7 +10,7 @@ use GDO\Core\GDT_Secret;
 use GDO\User\GDO_User;
 use GDO\Net\HTTP;
 use GDO\UI\GDT_Success;
-use GDO\Core\GDT_Error;
+use GDO\UI\GDT_Error;
 use GDO\UI\GDT_Button;
 use GDO\Core\GDT_Array;
 

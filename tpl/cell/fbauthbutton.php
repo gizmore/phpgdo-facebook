80b339b15e08adb24b537b71655fe2808261bec6
gizmore <gizmore@wechall.net>
2022-08-10 20:35:56 +0200

    '7.0.1-r1418'

--- tpl/cell/fbauthbutton.php
+++ tpl/cell/fbauthbutton.php
@@ -8,6 +8,6 @@ $icon = sprintf('<img src="%sGDO/Facebook/img/fb-btn.png"
  title="%s" style="width: 300px;" />',
  GDO_WEB_ROOT, t('btn_continue_with_fb'));
 
-echo GDT_Link::make()->noLabel()->
+echo GDT_Link::make()->labelNone()->
 	href($field->href)->rawIcon($icon)->
 	render();

d8811b4868087419f71b62d0e0e773d71c58389a
gizmore <gizmore@wechall.net>
2026-09-18 04:06:07 +0200

    'more korean'

---
+++ lang/facebook_ko.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'link_fb_auth' => '페이스북으로 가입',
+	'mt_facebook_auth' => '페이스북으로 계속하기',
+	'msg_facebook_connected' => 'Facebook 계정으로 성공적으로 인증되었습니다.',
+	'err_facebook_connect' => 'Facebook oAuth 인증 프로세스에 실패했습니다.',
+	'msg_fb_avatar_imported' => '귀하의 페이스북 아바타 이미지를 가져왔습니다.',
+	'err_fb_avatar_not_imported' => '귀하의 Facebook 아바타를 가져오지 못했습니다.',
+	'cfg_fb_auth' => 'Facebook 인증을 활성화하시겠습니까?',
+	'cfg_fb_app_id' => '페이스북 앱 ID',
+	'cfg_fb_secret' => 'Facebook 앱 비밀',
+];
